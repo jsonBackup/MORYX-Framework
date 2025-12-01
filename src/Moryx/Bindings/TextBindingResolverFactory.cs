a6cd11bc7876e0d413ee57bba36be060adda3b6b
jsonBackup <jb-2000@gmx.de>
2025-12-01 13:20:42 +0100

    Fix `DriverPropertiesTests` and `ParameterBindingTests`

--- src/Moryx/Bindings/TextBindingResolverFactory.cs
+++ src/Moryx/Bindings/TextBindingResolverFactory.cs
@@ -14,7 +14,7 @@ namespace Moryx.Bindings
         /// Regex for binding.
         /// It matches strings of type "Product.Id" or "Recipe".
         /// </summary>
-        private const string BindingRegex = @"\{(?<binding>([A-Za-z_][A-Za-z0-9_]*)(\[\d+\])?(\.([A-Za-z_][A-Za-z0-9_]*)(\[\d+\])?)*)?(?<hasFormat>:(?<format>\w+))?\}";
+        private const string BindingRegex = @"\{(?<binding>([A-Za-z_][A-Za-z0-9_]*)(\[\w+\])?(\.([A-Za-z_][A-Za-z0-9_]*)(\[\w+\])?)*)?(?<hasFormat>:(?<format>\w+))?\}";
 
         /// <summary>
         /// Create resolver for this instruction
